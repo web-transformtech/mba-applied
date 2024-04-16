e67fa59950fb0e339ad13dbc959b3e21f8b472cf
ThirunavukarasanA <thirunavukarasan@rvsgroup.com>
2024-04-16 20:00:41 +0530

    brochure

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const Loader = lazy(() => import('./components/Loader'));
 function App() {
     return (
         <div>
-            <BrowserRouter>
+            <BrowserRouter basename="/applied">
                 <Routes>
                     {/* <Route path="/" element={<HomePage />} /> */}
                     <Route
