9336c28c333c5f45a5bb44ee96e06b8335bce5f5
ThirunavukarasanA <thirunavukarasan@rvsgroup.com>
2024-04-17 10:51:14 +0530

    Updates

---
+++ assets/Digital Marketing-JcI0IQgh.js
@@ -0,0 +1,1 @@
+const a="/applied/assets/Digital Marketing-k7KG6Vak.pdf";export{a as B};
---
+++ assets/Loader-6mR9Toof.js
@@ -0,0 +1,1 @@
+import{j as e}from"./react-U3tOp64R.js";import"./jquery-rc8GnQ-j.js";function t(){return e.jsx("div",{className:"flex justify-center items-center h-[100vh]",children:e.jsx("div",{className:"loader"})})}export{t as default};
